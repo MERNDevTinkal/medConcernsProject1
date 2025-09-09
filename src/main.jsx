e2ebbd017bb928cda93c5a1eb6f0e38f655190b9
Aman0246jploft <amakashyap0246jploft@gmail.com>
2025-09-09 15:31:43 +0530

    add pwa code

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,16 @@ createRoot(document.getElementById("root")).render(
     </GlobalProvider>
   </StrictMode>
 );
+
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Refresh?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline");
+  },
+});
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,47 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config/
+import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "robots.txt"], // Optional, but recommended
+      manifest: {
+        name: "My React Vite PWA",
+        short_name: "VitePWA",
+        description: "A Vite PWA built with React",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          // {
+          //   src: "apple-touch-icon.png",
+          //   sizes: "180x180",
+          //   type: "image/png",
+          // },
+        ],
+      },
+    }),
+  ],
+  server: {
+    hmr: {
+      overlay: false,
+    },
+    host: true,
+  },
 });
