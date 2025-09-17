d29d2738e2e57c509b211b374fba330437cfbdf6
ashendra-wa <amakashyap0246jploft@gmail.com>
2025-09-17 11:44:45 +0530

    change name

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
       registerType: "autoUpdate",
       // includeAssets: ["favicon.ico", "apple-touch-icon.png", "robots.txt"], // Optional, but recommended
       manifest: {
-        name: "My React Vite PWA",
+        name: "MedConcerns App",
         short_name: "MedConcerns",
-        description: "A Vite PWA built with React",
+        description: "MedConcerns App",
         theme_color: "#ffffff",
         background_color: "#ffffff",
         display: "standalone",
