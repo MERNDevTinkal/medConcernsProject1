e04ee76e7e74194c4d9b4e5a23e1bd280500a285
ashendra-wa <ashendr.kushwha@jploft.in>
2025-11-12 19:05:12 +0530

    add files

--- src/pages/Board/NeedBoard.jsx
+++ src/pages/Board/NeedBoard.jsx
@@ -50,8 +50,8 @@ const NeedBoard = () => {
       setCalendarOn,
       setIntroductionOn,
       setLoader,
-      () => {},
-      () => {},
+      () => { },
+      () => { },
       setUncheckNeedBoard
     );
 
@@ -217,20 +217,20 @@ const NeedBoard = () => {
       const audioValue = value?.audio
         ? value?.audio
         : selectedLanguage === "" && selectedGender === ""
-        ? value?.maleEnglish
-        : selectedLanguage === "Spanish" && selectedGender === "Male"
-        ? value?.maleSpanish
-        : selectedLanguage === "Spanish" && selectedGender === "Female"
-        ? value?.femaleSpanish
-        : selectedLanguage === "" && selectedGender === "Female"
-        ? value?.femaleEnglish
-        : selectedLanguage === "" && selectedGender === "Male"
-        ? value?.maleEnglish
-        : selectedLanguage === "English" && selectedGender === "Male"
-        ? value?.maleEnglish
-        : selectedLanguage === "English" && selectedGender === "Female"
-        ? value?.femaleEnglish
-        : value?.maleEnglish;
+          ? value?.maleEnglish
+          : selectedLanguage === "Spanish" && selectedGender === "Male"
+            ? value?.maleSpanish
+            : selectedLanguage === "Spanish" && selectedGender === "Female"
+              ? value?.femaleSpanish
+              : selectedLanguage === "" && selectedGender === "Female"
+                ? value?.femaleEnglish
+                : selectedLanguage === "" && selectedGender === "Male"
+                  ? value?.maleEnglish
+                  : selectedLanguage === "English" && selectedGender === "Male"
+                    ? value?.maleEnglish
+                    : selectedLanguage === "English" && selectedGender === "Female"
+                      ? value?.femaleEnglish
+                      : value?.maleEnglish;
       await getTextToSpeech(
         selectedLanguage === "Spanish" ? value.nameEs : value.name,
         selectedLanguage === "Spanish" ? "es-ES" : "",
@@ -273,83 +273,83 @@ const NeedBoard = () => {
           />
 
           <div className="main-wrapper home-wrapper ">
-              <div className="dashboard-wrapper px-4 py-1.5">
-            <div
-              className="dashboard-h grid gap-3 p-3"
-              style={{
-                gridTemplateColumns:
-                  selectedIconCount === 6
-                    ? "repeat(3, 1fr)" // 3 per row
-                    : `repeat(${selectedIconCount || 2}, 1fr)`,
-                gridTemplateRows:
-                  selectedIconCount === 6 ? "repeat(2, 1fr)" : "auto",
-              }}
-            >
-              {mergedData
-                .filter((item) => !selectedNeedboard.includes(item.name))
-                .map((item, index) => (
-                  <div
-                            className={
-                      selectedIconCount === 1
-                        ? "dash-single-items"
-                        : selectedIconCount === 2
-                        ? "dash-double-items"
-                        : selectedIconCount === 3
-                        ? "dash-triple-items"
-                        : selectedIconCount === 4
-                        ? "dash-quadriple-items"
-                        : selectedIconCount === 6
-                        ? "dash-hexuple-items"
-                        : ""
-                    }
-                    style={{ cursor: "pointer" }}
-                    key={index}
-                  >
+            <div className="dashboard-wrapper px-4 py-1.5">
+              <div
+                className="dashboard-h grid gap-3 p-3"
+                style={{
+                  gridTemplateColumns:
+                    selectedIconCount === 6
+                      ? "repeat(3, 1fr)" // 3 per row
+                      : `repeat(${selectedIconCount || 2}, 1fr)`,
+                  gridTemplateRows:
+                    selectedIconCount === 6 ? "repeat(2, 1fr)" : "auto",
+                }}
+              >
+                {mergedData
+                  .filter((item) => !selectedNeedboard.includes(item.name))
+                  .map((item, index) => (
+                    <div
+                      className={
+                        selectedIconCount === 1
+                          ? "dash-single-items"
+                          : selectedIconCount === 2
+                            ? "dash-double-items"
+                            : selectedIconCount === 3
+                              ? "dash-triple-items"
+                              : selectedIconCount === 4
+                                ? "dash-quadriple-items"
+                                : selectedIconCount === 6
+                                  ? "dash-hexuple-items"
+                                  : ""
+                      }
+                      style={{ cursor: "pointer" }}
+                      key={index}
+                    >
                       <div className="dashboard-cards rounded-2xl bg-white text-center border-2 border-white hover:border-blue-600 shadow-sm transition-colors duration-300">
-                      {item.name && !item.nameEs && (
-                        <div className="flex justify-end absolute top-4 right-4">
-                          <span style={{ color: "blue" }}>
-                            <MdEdit
-                              onClick={() => {
-                                // handleEdit(item);
-                                navigate(`/board-upload/${item.id}`, {
-                                  state: { item },
-                                });
-                              }}
-                            />
-                          </span>
-                          <span style={{ color: "red" }}>
-                            <MdOutlineDelete
-                              onClick={() => {
-                                handleDelete(item.id);
-                              }}
-                            />
-                          </span>
+                        {item.name && !item.nameEs && (
+                          <div className="flex justify-end absolute top-4 right-4">
+                            <span style={{ color: "blue" }}>
+                              <MdEdit
+                                onClick={() => {
+                                  // handleEdit(item);
+                                  navigate(`/board-upload/${item.id}`, {
+                                    state: { item },
+                                  });
+                                }}
+                              />
+                            </span>
+                            <span style={{ color: "red" }}>
+                              <MdOutlineDelete
+                                onClick={() => {
+                                  handleDelete(item.id);
+                                }}
+                              />
+                            </span>
+                          </div>
+                        )}
+                        <div
+                          className="dashboard-img card-img-h rounded-2xl"
+                          onClick={() => handleNeedBoard(item, item.secPath)}
+                        >
+                          <img
+                            className="w-full"
+                            style={{
+                              height: selectedIconCount === 6 ? "" : "",
+                            }}
+                            src={item?.image}
+                            alt={item?.name}
+                          />
                         </div>
-                      )}
-                      <div
-                        className="dashboard-img card-img-h rounded-2xl"
-                        onClick={() => handleNeedBoard(item, item.secPath)}
-                      >
-                        <img
-                          className="mainImageSize"
-                          style={{
-                            height: selectedIconCount === 6 ? "" : "",
-                          }}
-                          src={item?.image}
-                          alt={item?.name}
-                        />
+                        <p className="text-[16px] mt-4 color-black mb-0 ">
+                          {selectedLanguage === "Spanish"
+                            ? item?.nameEs || item?.name
+                            : item?.name}
+                        </p>
                       </div>
-                      <p className="text-[16px] mt-4 color-black mb-0 ">
-                        {selectedLanguage === "Spanish"
-                          ? item?.nameEs || item?.name
-                          : item?.name}
-                      </p>
                     </div>
-                  </div>
-                ))}
+                  ))}
+              </div>
             </div>
-</div>
             {/* Add Button at bottom */}
             <div className="flex justify-center my-6">
               <button
