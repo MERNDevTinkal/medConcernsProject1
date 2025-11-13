09611b5e40587ab7e84be4895c47685522a5052f
ashendra-wa <ashendr.kushwha@jploft.in>
2025-11-13 10:24:11 +0530

    add header

--- src/Component/Layout/Header/Header.jsx
+++ src/Component/Layout/Header/Header.jsx
@@ -277,7 +277,7 @@ const Header = ({ selectedLanguage, introductionOn, calendarOn, name }) => {
                 <img src={hamburger} alt="menu" />
               </button>
             </div>
-            <h2 className="text-[25px] font-normal text-black">
+            <h2 className="text-[25px] font-normal text-black transform -translate-x-9">
               {name ??
                 (selectedLanguage === "Spanish"
                   ? "Preocupaciones"
