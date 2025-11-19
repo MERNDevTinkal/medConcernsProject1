25715844b2682bc7cc842dbfa0f0458eda414020
ashendra-wa <ashendr.kushwha@jploft.in>
2025-11-19 15:25:23 +0530

    add landscape skip

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { GlobalProvider } from "./context/DiseaseContext.jsx";
 import { registerSW } from "virtual:pwa-register";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </GlobalProvider>
   </StrictMode>
 );
